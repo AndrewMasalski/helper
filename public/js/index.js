5dc5e513693c44153b8cace12e9a0f0eac8a98bd
Andrew Masalski <amasalski@exadel.com>
2017-05-12 15:51:06 +0300

    fetch POST request

---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+function g(id) {
+    return document.getElementById(id);
+}
+
+function generateDecision() {
+    let question = g('input').value;
+    if (question) {
+        fetch('http://localhost:3000/answer')
+            .then(function(response) {
+                return response.json();
+            }).then(function(data) {
+            g('out').innerHTML = data;
+        }).catch(function(err) {
+            console.log(err);
+        })
+    } else {
+        g('out').innerHTML = 'No Question...'
+    }
+}
+
+function addNewOption() {
+    let newOption = g('add_modal_input').value;
+    console.log(newOption);
+    let message;
+    if (newOption) {
+        fetch('http://localhost:3000/new_option',
+            {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({text: newOption})
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                message = data.message;
+            })
+            .catch(function(err) {
+                message = err.message;
+            });
+    } else {
+        g('add_status').innerHTML = 'Nothing to send..'
+    }
+}
---
+++ server.js
@@ -0,0 +1,83 @@
+let express = require('express');
+let app = express();
+var cors = require('cors');
+
+let fs = require('fs');
+let db = fs.readFileSync('db.json');
+let obj = JSON.parse(db);
+
+let bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//root
+/*
+app.get('/', function (req, res) {
+	res.send('Landing page!');
+});
+*/
+
+//get answer
+app.get('/answer', sendAnswer);
+function sendAnswer(response) {
+	let answer;
+	let rand = Math.random() * 100;
+	if (rand < 50) {
+		answer = obj[0];
+	}else {
+		answer = obj[1];
+	}
+	response.send(JSON.stringify(answer));
+}
+
+app.post('/new_option', function (request, response) {
+	let data = request.body;
+	console.log(data);
+	response.send({added: data})
+});
+
+/*app.get('/add/:prop/:v?', function (request, response) {
+	let data = request.params;
+	let property = data.prop;
+	let value = data.v;
+	let replay;
+	if (!value) {
+		replay = {
+			msg: 'Value is required!!!'
+		}
+	}else {
+		obj[property] = value;
+		let toStorage = JSON.stringify(obj, null, 2);
+		fs.writeFile('db.json', toStorage, function (err) {
+			console.log('All set...')
+		});
+		replay = {
+			msg: 'Data saved!!'
+		}
+	}
+	response.send(replay);
+});*/
+
+
+/*app.get('/try/:property/', function (request, response) {
+	let data = request.params.property;
+	let replay;
+	if (obj[data]) {
+		replay = {
+			msg: 'property is here!'
+		}
+	}else {
+		replay = {
+			msg: 'Not Found'
+		}
+	}
+	response.send(replay);
+
+});*/
+
+let server = app.listen(3000, function () {
+	console.log('Online... Port: ' + server.address().port);
+});
